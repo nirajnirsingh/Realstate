1125e01821313291f7d1efa4c0e6e5d0073cffd2
Niraj Singh <csaiml22131@glbitm.ac.in>
2025-07-17 17:59:08 +0530

    inial commit

--- server/config/connectDB.js
+++ server/config/connectDB.js
@@ -1,12 +1,22 @@
 import mongoose from "mongoose";
-import dotenv from "dotenv"
-dotenv.config()
-const URI = process.env.MONGO
+import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
+
+// Setup __dirname manually
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Load .env from parent folder
+dotenv.config({ path: path.resolve(__dirname, "../.env") });
+
+const URI = process.env.MONGO_URI;
+console.log("MONGO_URI =", URI); // Confirm it's loaded
 
 const connectDB = async () => {
   try {
     await mongoose.connect(URI);
-    console.log('MongoDB Connected...');
+    console.log("MongoDB Connected...");
   } catch (err) {
     console.error(err.message);
     process.exit(1);
